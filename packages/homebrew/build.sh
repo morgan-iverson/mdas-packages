d6d8ee5dc6465de65bc0ce8728563db98a95927d
morgan-iverson <miverson@vmware.com>
2022-11-01 13:12:40 -0700

    Unzip into Image

--- packages/homebrew/build.sh
+++ packages/homebrew/build.sh
@@ -10,5 +10,12 @@ HOMEBREW_TARGET=$PROJECT_DIR/homebrew/homebrew/Homebrew
 
 mkdir -p $PROJECT_DIR/homebrew/homebrew
 
+
+cd $PROJECT_DIR/homebrew
+# exit 1
 # INSTALL & UNPACK
 curl -L -o $HOMEBREW_TARGET.tar.gz https://github.com/Homebrew/brew/tarball/master
+tar -xvf $HOMEBREW_TARGET.tar.gz --strip 1
+# eval "$($(pwd)/bin/brew shellenv)" 
+# brew update --force --quiet
+# chmod -R go-w "$(brew --prefix)/share/zsh"
--- packages/homebrew/setup.d/setup.sh
+++ packages/homebrew/setup.d/setup.sh
@@ -9,12 +9,12 @@ HOMEBREW_TARGET=$PROJECT_DIR/homebrew/Homebrew
 
 
 cd $PROJECT_DIR/homebrew
-tar -xvf $HOMEBREW_TARGET.tar.gz --strip 1
+# tar -xvf $HOMEBREW_TARGET.tar.gz --strip 1
 eval "$($(pwd)/bin/brew shellenv)" 
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 
-rm HOMEBREW_TARGET.tar.gz
+rm $HOMEBREW_TARGET.tar.gz
 
 HOMEBREW_HOME=$(pwd)
 PATH=$HOMEBREW/bin:$PATH
