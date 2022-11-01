42128f61af05947bef0e2a2fd9e71ab76a0e734d
morgan-iverson <miverson@vmware.com>
2022-11-01 11:58:54 -0700

    Strip unzip

--- packages/homebrew/setup.d/setup.sh
+++ packages/homebrew/setup.d/setup.sh
@@ -6,13 +6,18 @@ set -eo pipefail
 PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd) # /opt/packages/homebrew/
 
 HOMEBREW_TARGET=$PROJECT_DIR/homebrew/Homebrew
+# HOMEBREW_HOME=$PROJECT_DIR/homebrew/Homebrew
 
 
 cd $PROJECT_DIR/homebrew
-tar -xvf $HOMEBREW_TARGET.tar.gz
+tar -xvf --strip 1 $HOMEBREW_TARGET.tar.gz
 # eval "$(homebrew/bin/brew shellenv)" # ??
 # brew update --force --quiet
 # chmod -R go-w "$(brew --prefix)/share/zsh"
+
+# HOMEBREW_HOME=$HOMEBREW_TARGET/bin
+# PATH=$HOMW
+
 
 ##  ORIGINAL
 # mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
