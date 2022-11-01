47705369e6bafe7356a2f355cb4062d441065bf7
morgan-iverson <miverson@vmware.com>
2022-11-01 12:44:17 -0700

    Fix eval bin diractory

--- packages/homebrew/setup.d/setup.sh
+++ packages/homebrew/setup.d/setup.sh
@@ -10,11 +10,11 @@ HOMEBREW_TARGET=$PROJECT_DIR/homebrew/Homebrew
 
 cd $PROJECT_DIR/homebrew
 tar -xvf $HOMEBREW_TARGET.tar.gz --strip 1
-eval "$(homebrew/bin/brew shellenv)" 
+eval "$($(pwd)/bin/brew shellenv)" 
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
 
-rm HOMEBREW_TARGET
+rm HOMEBREW_TARGET.tar.gz
 
 HOMEBREW_HOME=$(pwd)
 PATH=$HOMEBREW/bin:$PATH
