41709a211ec9049b105a1884dbe008d312bb334f
morgan-iverson <miverson@vmware.com>
2022-11-01 12:37:12 -0700

    Add to path, Remove .tag.gz

--- packages/homebrew/setup.d/setup.sh
+++ packages/homebrew/setup.d/setup.sh
@@ -6,22 +6,15 @@ set -eo pipefail
 PROJECT_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd) # /opt/packages/homebrew/
 
 HOMEBREW_TARGET=$PROJECT_DIR/homebrew/Homebrew
-# HOMEBREW_HOME=$PROJECT_DIR/homebrew/Homebrew
 
 
 cd $PROJECT_DIR/homebrew
 tar -xvf $HOMEBREW_TARGET.tar.gz --strip 1
-# eval "$(homebrew/bin/brew shellenv)" # ??
-# brew update --force --quiet
-# chmod -R go-w "$(brew --prefix)/share/zsh"
+eval "$(homebrew/bin/brew shellenv)" 
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
 
-# HOMEBREW_HOME=$HOMEBREW_TARGET/bin
-# PATH=$HOMW
+rm HOMEBREW_TARGET
 
-
-##  ORIGINAL
-# mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-# eval "$(homebrew/bin/brew shellenv)"
-# brew update --force --quiet
-# chmod -R go-w "$(brew --prefix)/share/zsh"
-# remove .tgz
+HOMEBREW_HOME=$(pwd)
+PATH=$HOMEBREW/bin:$PATH
